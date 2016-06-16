a9c5d731c1f52e2966bc74586c53f92b1445d5b5
Juan Sepulveda <juan@lavaina.com>
2016-06-16 13:57:49 -0400

    Use  in test script

--- meteor/bin/test.sh
+++ meteor/bin/test.sh
@@ -1,83 +1,58 @@
 #!/bin/bash -x
 
-export PHANTHOMJS_SCRIPT="$TRAVIS_BUILD_DIR/meteor/tests/phantomjs-script-tests.js"
-export TEST_FILE="$TRAVIS_BUILD_DIR/meteor/tests/tests.html"
+export PHANTHOMJS_SCRIPT="$REPO_HOME/meteor/tests/phantomjs-script-tests.js"
+export TEST_FILE="$REPO_HOME/meteor/tests/tests.html"
 export SPACEJAM_COMMAND="--phantomjs-script $PHANTHOMJS_SCRIPT  --driver-package practicalmeteor:mocha"
 
-cd $TRAVIS_BUILD_DIR
-spacejam test-packages ./
+function verifyStatus {
+  EXIT_STATUS=0
+  if [ "$1" -ne "0" ]
+    then
+    if [ -n "$TRAVIS_BUILD_DIR" ]
+      then
+      echo "Test failed printing compare file."
+      cat "$TEST_FILE.compare"
+      echo "End compare file."
+    else
+      echo "Test failed creating compare file."
+      cat "$TEST_FILE.compare" >> "$(dirname $TEST_FILE)/$2".compare.html
+    fi
+    EXIT_STATUS="$1"
+  fi
+  return ${EXIT_STATUS}
+}
+
+cd $REPO_HOME
+#spacejam test-packages ./
 EXIT_STATUS="$?"
 
-cd "$TRAVIS_BUILD_DIR/meteor/test-app"
+cd "$REPO_HOME/meteor/test-app"
 spacejam test $(echo $SPACEJAM_COMMAND)
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
-cd "$TRAVIS_BUILD_DIR/meteor/test-app"
+cd "$REPO_HOME/meteor/test-app"
 MOCHA_RUN_ORDER='serial'
 spacejam test $(echo $SPACEJAM_COMMAND)
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
-
-cd "$TRAVIS_BUILD_DIR/meteor/test-app"
+cd "$REPO_HOME/meteor/test-app"
 MOCHA_RUN_ORDER='parallel'
 spacejam test --full-app $(echo $SPACEJAM_COMMAND)
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
-cd "$TRAVIS_BUILD_DIR/meteor/test-app"
+cd "$REPO_HOME/meteor/test-app"
 MOCHA_RUN_ORDER='serial'
 spacejam test --full-app $(echo $SPACEJAM_COMMAND)
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
-cd "$TRAVIS_BUILD_DIR/meteor/test-package"
+cd "$REPO_HOME/meteor/test-app/packages/test-package"
 MOCHA_RUN_ORDER='parallel'
 spacejam test-packages $(echo $SPACEJAM_COMMAND) ./
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
-cd "$TRAVIS_BUILD_DIR/meteor/test-package"
+cd "$REPO_HOME/meteor/test-app/packages/test-package"
 MOCHA_RUN_ORDER='serial'
 spacejam test-packages $(echo $SPACEJAM_COMMAND) ./
-LAST_EXIT_STATUS="$?"
-if [ "$LAST_EXIT_STATUS" -ne "0" ]
-    then
-    echo "Test failed printing compare file."
-    cat "$TEST_FILE.compare"
-    echo "End compare file."
-    EXIT_STATUS="$LAST_EXIT_STATUS"
-fi
+EXIT_STATUS=$(verifyStatus "$?" "test-app-parallel")
 
 exit $EXIT_STATUS
