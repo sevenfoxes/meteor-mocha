ea9e51756487e2913239e7dd7c360b3533007aa7
Juan Sepulveda <juan@lavaina.com>
2016-06-17 20:33:00 -0400

    Add install-meteor.sh script

---
+++ meteor/bin/install-meteor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+if [ ! -e "$HOME/.meteor/meteor" ]; then
+  curl https://install.meteor.com/ | sh
+else
+  echo "meteor already exists in Travis CI cache, not installing it."
+fi
+
+# Making sure correct version of meteor is downloaded
+meteor --release 1.3.3 --version
