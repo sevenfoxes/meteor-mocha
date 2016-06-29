d0be041ae62ac3937a7502a6b572c0147a1d5c81
Juan Sepulveda <juan@lavaina.com>
2016-06-29 17:24:08 -0400

    Bump version to 2.4.5_5

--- meteor/src/lib/MochaRunner.coffee
+++ meteor/src/lib/MochaRunner.coffee
@@ -17,7 +17,7 @@ class MochaRunner extends EventEmitter
   @get: ->
     MochaRunner.instance ?= new MochaRunner()
 
-  VERSION: "2.4.5-rc5.5"
+  VERSION: "2.4.5_5"
   serverRunEvents: null
   publishers: {}
 
--- meteor/test-package/package.js
+++ meteor/test-package/package.js
@@ -24,7 +24,7 @@ Package.onTest(function (api) {
     'coffeescript',
     'practicalmeteor:loglevel',
     'practicalmeteor:chai',
-    'practicalmeteor:mocha@=2.4.5-rc5.5',
+    'practicalmeteor:mocha@=2.4.5_5',
     'ecmascript',
     'test-package'
   ]);
